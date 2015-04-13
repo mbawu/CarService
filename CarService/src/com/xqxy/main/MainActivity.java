faff31737c7a10c0dc586f8673a36991cc5057d0
Administrator <Administrator@10.9.50.126>
2015-04-13 14:07:29 +0800

    fsfs

--- CarService/src/com/xqxy/main/MainActivity.java
+++ CarService/src/com/xqxy/main/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);//111222444
+		setContentView(R.layout.activity_main);//1112225555
 	}
 
 	@Override
