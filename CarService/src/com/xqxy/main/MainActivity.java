c90c23661af91632f3fc5bedc26e241091d9eb76
ChangHong_XXX <ChangHong_XXX@10.9.50.84>
2015-04-13 14:10:19 +0800

    test5

--- CarService/src/com/xqxy/main/MainActivity.java
+++ CarService/src/com/xqxy/main/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);//111222444
+		setContentView(R.layout.activity_main);//11122266
 	}
 
 	@Override
